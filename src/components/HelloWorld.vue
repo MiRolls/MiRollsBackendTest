a9913ddc46328697a740ff4b7dbf811524c81b4b
liangmiQwQ <lm@lmfans.cn>
2023-12-19 18:09:44 +0800

    feat: axios and fetch choose

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,26 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import {ref} from 'vue'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+const server = ref("")
+const axios = ref("axios")
+const send = (link: string, body: any): boolean => {
+    return true
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <h1>{{ msg }}</h1>
+    <span>All the things will print in the console</span>
+    <span>所有的返回信息都会呈现在控制台中</span>
+    <div>Axios<input v-model="axios" type="radio" value="axios"></div>
+    <div>Fetch<input v-model="axios" type="radio" value="fetch"></div>
+    <div>
+        <input type="text" placeholder="Server Domain/IP" v-model="server">
+    </div>
+    <button type="button" @click="send">Send</button>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
 </style>
